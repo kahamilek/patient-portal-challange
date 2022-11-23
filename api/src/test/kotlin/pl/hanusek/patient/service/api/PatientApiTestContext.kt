e200637f3f45db3572253ca719463d3662eff2de
hanusek <kamil.hanusek@jakdojade.pl>
2022-11-23 23:45:57 +0100

    Create test context for patient api

---
+++ api/src/test/kotlin/pl/hanusek/patient/service/api/PatientApiTestContext.kt
@@ -0,0 +1,15 @@
+package pl.hanusek.patient.service.api
+
+import org.springframework.http.ResponseEntity
+import pl.hanusek.patient.service.api.dto.CreatePatientRequestDto
+import pl.hanusek.patient.service.api.dto.CreatePatientResponseDto
+import pl.hanusek.patient.service.domain.patient.PatientTestContext
+
+class PatientApiTestContext {
+    fun createPatient(createPatientRequestDto: CreatePatientRequestDto): ResponseEntity<CreatePatientResponseDto> {
+        return patientsController.createPatient(createPatientRequestDto)
+    }
+
+    private val patientsFacade = PatientTestContext().patientsFacade
+    private val patientsController = PatientsController(patientsFacade)
+}
