d7c7ceb950dc88166bd30b4248e4d6384f8ebc65
hanusek <kamil.hanusek@jakdojade.pl>
2022-11-24 14:45:42 +0100

    Reorganize code in PatientApiTestContext

--- api/src/test/kotlin/pl/hanusek/patient/service/api/PatientApiTestContext.kt
+++ api/src/test/kotlin/pl/hanusek/patient/service/api/PatientApiTestContext.kt
@@ -6,10 +6,12 @@ import pl.hanusek.patient.service.api.dto.CreatePatientResponseDto
 import pl.hanusek.patient.service.domain.patient.PatientTestContext
 
 class PatientApiTestContext {
+
+    private val patientsFacade = PatientTestContext().patientsFacade
+    private val patientsController = PatientsController(patientsFacade)
+
     fun createPatient(createPatientRequestDto: CreatePatientRequestDto): ResponseEntity<CreatePatientResponseDto> {
         return patientsController.createPatient(createPatientRequestDto)
     }
 
-    private val patientsFacade = PatientTestContext().patientsFacade
-    private val patientsController = PatientsController(patientsFacade)
 }
