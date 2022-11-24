9bff1585c0a6876e1234810137ace411be7a994b
kahamilek <37459485+kahamilek@users.noreply.github.com>
2022-11-25 00:17:58 +0100

    Fix/fix changing doctor's appointment time (#9)
    * Add doctors-appointment id to response model
    
    * Fix logic for changing doctor's appointment time
    
    Co-authored-by: hanusek <kamil.hanusek@jakdojade.pl>

--- api/src/main/kotlin/pl/hanusek/patient/service/api/doctor_appointment/DoctorsAppointmentRestController.kt
+++ api/src/main/kotlin/pl/hanusek/patient/service/api/doctor_appointment/DoctorsAppointmentRestController.kt
@@ -3,10 +3,7 @@ package pl.hanusek.patient.service.api.doctor_appointment
 import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import pl.hanusek.patient.service.api.doctor_appointment.dto.CreateDoctorsAppointmentRequestDto
-import pl.hanusek.patient.service.api.doctor_appointment.dto.CreateDoctorsAppointmentResponseDto
-import pl.hanusek.patient.service.api.doctor_appointment.dto.DoctorsAppointmentDto
-import pl.hanusek.patient.service.api.doctor_appointment.dto.GetDoctorsAppointmentsResponseDto
+import pl.hanusek.patient.service.api.doctor_appointment.dto.*
 import pl.hanusek.patient.service.domain.FullName
 import pl.hanusek.patient.service.domain.OrderType
 import pl.hanusek.patient.service.domain.Pageable
@@ -46,12 +43,12 @@ class DoctorsAppointmentRestController(
     @PutMapping("/api/v1/doctors-appointment/{appointmentId}")
     fun changeTimeOfAppointment(
         @PathVariable appointmentId: String,
-        newTime: LocalTime
+        @RequestBody requestDto: ChangeDoctorsAppointmentTimeRequestDto
     ): DoctorsAppointmentDto {
         return doctorsAppointmentsFacade.changeTimeOfDoctorsAppointment(
             DoctorsAppointment.DoctorsAppointmentId.from(
                 appointmentId
-            ), newTime
+            ), requestDto.newTime
         )
             .toDtoModel()
     }
@@ -87,6 +84,7 @@ private fun  SinglePage<DoctorsAppointmentsFacade.EnrichedDoctorsAppointment>.to
 
 private fun DoctorsAppointmentsFacade.EnrichedDoctorsAppointment.toDtoModel(): DoctorsAppointmentDto {
     return DoctorsAppointmentDto(
+        id = this.doctorsAppointment.id.value,
         date = this.doctorsAppointment.date,
         time = doctorsAppointment.time,
         place = doctorsAppointment.place.value,
--- api/src/main/kotlin/pl/hanusek/patient/service/api/doctor_appointment/dto/DoctorsAppointmentDto.kt
+++ api/src/main/kotlin/pl/hanusek/patient/service/api/doctor_appointment/dto/DoctorsAppointmentDto.kt
@@ -1,10 +1,12 @@
 package pl.hanusek.patient.service.api.doctor_appointment.dto
 
 import pl.hanusek.patient.service.api.FullName
+import pl.hanusek.patient.service.domain.doctors_appointment.DoctorsAppointment
 import java.time.LocalDate
 import java.time.LocalTime
 
 data class DoctorsAppointmentDto(
+    val id: String,
     val date: LocalDate,
     val time: LocalTime,
     val place: String,
