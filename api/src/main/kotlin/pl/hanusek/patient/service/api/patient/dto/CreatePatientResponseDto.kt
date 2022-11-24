7413e55f572f67cbf878143c6126c38809092ae4
hanusek <kamil.hanusek@jakdojade.pl>
2022-11-24 16:13:04 +0100

    Fix property name to state instead of type

--- api/src/main/kotlin/pl/hanusek/patient/service/api/patient/dto/CreatePatientResponseDto.kt
+++ api/src/main/kotlin/pl/hanusek/patient/service/api/patient/dto/CreatePatientResponseDto.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName
 @JsonTypeInfo(
     use = JsonTypeInfo.Id.NAME,
     include = JsonTypeInfo.As.PROPERTY,
-    property = "type"
+    property = "state"
 )
 sealed class CreatePatientResponseDto {
 
