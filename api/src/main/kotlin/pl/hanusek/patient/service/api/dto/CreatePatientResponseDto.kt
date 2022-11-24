9603aa4f1a9cdfb123fe8d52cdc209154d569394
hanusek <kamil.hanusek@jakdojade.pl>
2022-11-24 15:04:57 +0100

    Add needed annotations to CreatePatientResponseDto class

--- api/src/main/kotlin/pl/hanusek/patient/service/api/dto/CreatePatientResponseDto.kt
+++ api/src/main/kotlin/pl/hanusek/patient/service/api/dto/CreatePatientResponseDto.kt
@@ -1,11 +1,21 @@
 package pl.hanusek.patient.service.api.dto
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeName
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
 sealed class CreatePatientResponseDto {
 
+    @JsonTypeName("SUCCESS")
     data class Success(
         val patientId: String
     ): CreatePatientResponseDto()
 
+    @JsonTypeName("ERROR")
     data class Error(
         val localizedMessage: String
     ): CreatePatientResponseDto()
