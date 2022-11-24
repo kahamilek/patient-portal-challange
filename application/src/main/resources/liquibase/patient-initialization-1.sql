d3a610a38af12596d19fd977fb632d77ae4d33ec
hanusek <kamil.hanusek@jakdojade.pl>
2022-11-24 16:59:25 +0100

    Change patient table schema

--- application/src/main/resources/liquibase/patient-initialization-1.sql
+++ application/src/main/resources/liquibase/patient-initialization-1.sql
@@ -1,13 +1,13 @@
 CREATE TABLE patient
 (
-    id              TEXT   NOT NULL PRIMARY KEY,
-    sequence_id     SERIAL NOT NULL,
-    full_name       JSON   NOT NULL,
-    address         JSON   NOT NULL,
-    organization_id TEXT   NOT NULL
+    id                 TEXT        NOT NULL PRIMARY KEY,
+    creation_timestamp TIMESTAMPTZ NOT NULL,
+    full_name          JSON        NOT NULL,
+    address            JSON        NOT NULL,
+    organization_id    TEXT        NOT NULL
 );
 
-CREATE INDEX ON patient (sequence_id);
+CREATE INDEX ON patient (creation_timestamp);
 
 CREATE TABLE organization
 (
